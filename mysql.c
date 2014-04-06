cbbd4e44b0f71b8ee578f5036bd511076859405a
U-keerath-VAIO\keerath <keerath@keerath-VAIO.(none)>
2014-04-06 12:00:27 +0530

    new record feature added

--- mysql.c
+++ mysql.c
@@ -5,7 +5,10 @@
 #include <string.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <windows.h>
 #define PORT_NUMBER 9080
+#define MAX_RECORD_LENGTH 1024
+#define MAX_VALUE_LENGTH 50
 json_error_t error;
 MYSQL *conn;
 json_t *arr;
@@ -25,41 +28,49 @@ void init()
     char *s;
     char *fields[100];
     conn = mysql_init(NULL);
-    /* Connect to database */
+/* Connect to database */
     if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
     {
         fprintf(stderr, "%s\n", mysql_error(conn));
         exit(1);
     }
+
     mysql_query(conn, "show fields from details");
     res1 = mysql_store_result(conn);
+
     while ((row1 = mysql_fetch_row(res1)) != NULL)
     {
         fields[j] = row1[0];
         j++;
     }
+
     mysql_query(conn,"ALTER TABLE details ADD COLUMN num INT AUTO_INCREMENT UNIQUE FIRST");
     mysql_query(conn,"CREATE TABLE temp LIKE details");
     mysql_query(conn,"INSERT INTO temp (SELECT * FROM details)");
     mysql_query(conn,"ALTER TABLE temp DROP COLUMN num");
     mysql_query(conn,"SELECT * FROM temp");
     res2 = mysql_use_result(conn);
+
     while ((row2 = mysql_fetch_row(res2)) != NULL)
     {
         json_t *obj = json_object();
+
         for (i = 0 ; i < mysql_num_fields(res2) ; i++)
         {
             json_object_set(obj, fields[i], json_string_nocheck(row2[i]));
-        
+
         }
         json_array_append(arr, obj);
     }
-       mysql_query(conn,"DROP TABLE temp");
+    mysql_query(conn,"DROP TABLE temp");
+
+    return;
 }
 void update_data(httpd *server)
 {
     httpVar *var2,*num,*field;
     char *str;
+
     if(strcmp(httpdRequestMethodName(server),"POST")==0)
     {  
         var2 = httpdGetVariableByName(server,"celldata");
@@ -70,94 +81,147 @@ void update_data(httpd *server)
         printf("%s",str);
         mysql_query(conn,str);
     }
+    return;
 }
 
 /*C function to dump and recieve the data. */
 void get_data(httpd *server)
 {       
-        json_t *col_data;
-        int i;
-        char *st;
-    httpVar *col = malloc(sizeof(httpVar*));
+    json_t *col_data;
+    int i;
+    char *st;
+    httpVar *col;
     col_data = json_array();
+
     if(strcmp(httpdRequestMethodName(server),"GET")==0)
     {
         httpdPrintf(server,"%s",json_dumps(arr,JSON_INDENT(4)));
+        printf("%s",json_dumps(arr,JSON_INDENT(4)));
     }
+
     else if(strcmp(httpdRequestMethodName(server),"POST")==0)
     {
         col= httpdGetVariableByName(server,"colname");
         st = col->value;
         col_data = json_loads(st,0,&error);
+
         for(i=0;i<json_array_size(col_data);i++)
         {       
-                json_t *col_obj,*st1;
-                char *string1,*string2;
-                st1 = json_object();
-                col_obj=json_array_get(col_data,i);
-                sprintf(string1,"%d",i+1);
-                st1 = json_object_get(col_obj,string1);
-                sprintf(string2,"ALTER TABLE details DROP COLUMN %s",json_string_value(st1));
-                if(mysql_query(conn,string2))
-                {
-                    fprintf(stdout,"%s",mysql_error(conn));
-                }
+            json_t *col_obj,*st1;
+            char *string1,*string2;
+            st1 = json_object();
+            col_obj=json_array_get(col_data,i);
+            sprintf(string1,"%d",i+1);
+            st1 = json_object_get(col_obj,string1);
+            sprintf(string2,"ALTER TABLE details DROP COLUMN %s",json_string_value(st1));
+
+            if(mysql_query(conn,string2))
+            {
+                fprintf(stdout,"%s",mysql_error(conn));
+            }
         }
     }
+    return;
+}
+void new_data(httpd *server)
+{   httpVar *insert_record;
+    int j=0;
+    json_t *new_obj =json_object();
+    const char *key;
+    json_t *value;
+    char *new_keys,*new_values,*query;
+    char temp[MAX_VALUE_LENGTH];
+
+    if(strcmp(httpdRequestMethodName(server),"POST")==0)
+    {   
+        insert_record = httpdGetVariableByName(server,"new_data");
+        new_obj = json_loads(insert_record->value,0,&error);
+        new_keys = calloc(MAX_RECORD_LENGTH,sizeof(char)) ;
+        new_values = calloc(MAX_RECORD_LENGTH,sizeof(char));
+        if(new_keys==NULL||new_values==NULL)
+            fprintf(stderr,"Memory Not Allocated");
+        json_object_foreach(new_obj,key,value)
+        {   
+            j++; 
+            strcat(new_keys,key);
+            sprintf(temp,"'%s'",json_string_value(value));
+            strcat(new_values,temp);
+
+            if(j!=json_object_size(new_obj))
+            {
+                strcat(new_keys,",");
+                strcat(new_values,",");
+            }
+        }
+           sprintf(query,"INSERT INTO details (%s) VALUES (%s)",new_keys,new_values);
+
+           if(mysql_query(conn,query))
+                fprintf(stderr,"%s",mysql_error(conn));
+
+           free(new_keys);
+           free(new_values);
+        
+        }
+    return;
 }
 void drop()
- {   
-    if(mysql_query("ALTER TABLE details DROP COLUMN num"))
-    {
+{   
+    if(mysql_query(conn,"ALTER TABLE details DROP COLUMN num"))
+    
         fprintf(stdout,"%s",mysql_error(conn));
-    }
- }
+        return;
+}
 void sig_handler(int signo)
 {   
     if(signo== SIGINT||signo== SIGABRT||signo==SIGKILL||signo==SIGQUIT)
         exit(1);
+        return;
 }
 /* Initialising HTTPD Server and mapping File Names to URLs. */
 int main(argc,argv)
 { 
     httpd *server;
-server = httpdCreate(NULL,PORT_NUMBER);
-if(server==NULL)
-{
-   perror("Cant Create Server");
-}
+    server = httpdCreate(NULL,PORT_NUMBER);
 
-signal(SIGINT,sig_handler);
-signal(SIGABRT,sig_handler);
-signal(SIGKILL,sig_handler);
-signal(SIGQUIT,sig_handler);
-atexit(drop); 
-httpdSetAccessLog(server,stdout);
-httpdSetErrorLog(server,stderr);
-httpdSetFileBase(server,"/cygdrive/c/users/keerath/desktop/project");
-httpdAddWildcardContent(server,"/icons",NULL,"icons");
-httpdAddWildcardContent(server,"/styles",NULL,"styles");
-httpdAddWildcardContent(server,"/styles/css",NULL,"styles/css");
-httpdAddWildcardContent(server,"/styles/fonts",NULL,"styles/fonts");
-httpdAddWildcardContent(server,"/",NULL,"");
-httpdAddFileContent(server,"/","index.html",HTTP_TRUE,NULL,"table.html");
-httpdAddCContent(server,"/","get_data",HTTP_FALSE,NULL,get_data);
-httpdAddCContent(server,"/","update_data",HTTP_FALSE,NULL,update_data);
-init();
-while(1==1)
-{
-   if(httpdGetConnection(server,0)<0)
-      continue;
-   if(httpdReadRequest(server)<0)
-   { 
-      httpdEndRequest(server);
-      continue;
-   }
-   else
-   {
-   httpdProcessRequest(server);
-   httpdEndRequest(server);
-}
-}
-return 0;
+    if(server==NULL)
+    {
+        perror("Cant Create Server");
+    }
+
+    signal(SIGINT,sig_handler);
+    signal(SIGABRT,sig_handler);
+    signal(SIGKILL,sig_handler);
+    signal(SIGQUIT,sig_handler);
+    atexit(drop); 
+    httpdSetAccessLog(server,stdout);
+    httpdSetErrorLog(server,stderr);
+    httpdSetFileBase(server,"/cygdrive/c/users/keerath/desktop/project");
+    httpdAddWildcardContent(server,"/icons",NULL,"icons");
+    httpdAddWildcardContent(server,"/styles",NULL,"styles");
+    httpdAddWildcardContent(server,"/styles/css",NULL,"styles/css");
+    httpdAddWildcardContent(server,"/styles/fonts",NULL,"styles/fonts");
+    httpdAddWildcardContent(server,"/scripts",NULL,"scripts");
+    httpdAddWildcardContent(server,"/",NULL,"");
+    httpdAddFileContent(server,"/","index.html",HTTP_TRUE,NULL,"table.html");
+    httpdAddCContent(server,"/","get_data",HTTP_FALSE,NULL,get_data);
+    httpdAddCContent(server,"/","update_data",HTTP_FALSE,NULL,update_data);
+    httpdAddCContent(server,"/","new_data",HTTP_FALSE,NULL,new_data);
+    init();
+
+    while(1==1)
+    {
+        if(httpdGetConnection(server,0)<0)
+            continue;
+        if(httpdReadRequest(server)<0)
+        { 
+            httpdEndRequest(server);
+            continue;
+        }
+        else
+        {
+            httpdProcessRequest(server);
+            httpdEndRequest(server);
+        }
+    }
+    return 0;
 }
---
+++ scripts/db.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $.getJSON("/get_data", function(data) {
+        var i=0 ;
+       $.each(data[0],function(key,value){
+        $("#headContainer").append("<th>" + key + "</th>");
+
+     $("#userInput").append("<h5 align = 'left'>&nbsp;Enter " + key + ":&nbsp;" 
+                        + "<input class = 'form-control up' name = "+ i +"  align='left' type='text'\
+                        placeholder='" + key + "'/></h5>");
+        i++;
+       });
+
+        for (var i in data) {
+            $("#table").append("<tr>  </tr>");
+            $.each(data[i], function(key, value) {
+                $("tr:last").append("<td>" + value + "</td>");
+               
+            });
+        }
+    });
+    $("#info").append("<button type = 'button' title = 'Add Records'\
+                     class = 'glyphicon glyphicon-plus-sign btn btn-info btn-sm highlight '\
+                     id='update'>&nbsp;Add</buttton> ");
+
+});
